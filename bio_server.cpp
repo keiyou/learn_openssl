170176ac1a120c6f970780c59defadc14485cf98
nichijou@pc <nichijou@DESKTOP-GML2TOI.localdomain>
2021-04-16 11:19:26 +0800

    bio_server multithread

--- bio_server.cpp
+++ bio_server.cpp
@@ -4,32 +4,69 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 1024
-#define SERVER_ADDR "localhost:8888"
+#define SERVER_ADDR "localhost:8088"
 
-
+void process_handle(BIO *connection);
+void* handle(void* arg);
 
 
 int main(){
-    BIO *sbio = NULL;
+    BIO *accept = NULL;
     char *addr = NULL;
+    pthread_t tid;
 
-    sbio = BIO_new_accept(SERVER_ADDR);
-    int acceptOk = BIO_do_accept(sbio);
-    if(acceptOk <= 0){
-        
+    accept = BIO_new_accept(SERVER_ADDR);
+
+    int accept_ok = BIO_do_accept(accept);
+    if(accept_ok <= 0){
+        printf("failed to set up accept");
+        exit(1);
     }
+
 
     while(true){
-        memset(out, 0, OUT_SIZE);
-        len = BIO_read(sbio, out, 80);
-        if(out[0] == 'q'){
-            break;
+        accept_ok = BIO_do_accept(accept);
+        if(accept_ok <= 0){
+            printf("failed to accept new connection");
+            exit(1);
         }
-        printf("%s", out);
+
+        BIO *connection = BIO_pop(accept);
+        int thread_ok = pthread_create(&tid, NULL, handle, connection);
+        if(thread_ok < 0){
+            printf("failed to spwan new thread");
+            exit(1);
+        }
     }
 
-    BIO_free(sbio);
+    BIO_free(accept);
     return 0;
+}
+
+
+void* handle(void *arg){
+    BIO* connection = (BIO *)arg;
+    process_handle(connection);
+    pthread_exit(NULL);
+}
+
+void process_handle(BIO* connection){
+    int len;
+    char recvBuf[BUFFER_SIZE];
+
+    printf("connection established\n");
+
+    while(true){
+        memset(recvBuf, 0, BUFFER_SIZE);
+        len = BIO_read(connection, recvBuf, BUFFER_SIZE);
+        if(len <= 0){
+            break;
+        }
+        printf("%s", recvBuf);
+    }
+    
+    BIO_free(connection);
 }
